286cfb9ec97ac6293688380cab45d368029c1370
Cristo Manuel Glez Delgado <113533760+CrisGlz@users.noreply.github.com>
2023-06-07 16:41:41 +0100

    Update Main.java

--- deposito/Main.java
+++ deposito/Main.java
@@ -1,5 +1,3 @@
-package cuentas;
-
 public class Main {
 
     /**
@@ -8,17 +6,25 @@ public class Main {
      * @param cantidad Cantidad a ingresar en la cuenta.
      */
     public static void operativa_cuenta(float cantidad) {
+        // Crear una instancia de la clase CCuenta llamada cuenta1
         CCuenta cuenta1 = new CCuenta("Antonio López", "1000-2365-85-1230456789", 2500, 0);
+        
+        // Obtener el saldo actual de la cuenta y mostrarlo en la consola
         double saldoActual = cuenta1.estado();
         System.out.println("El saldo actual es " + saldoActual);
 
         try {
+            // Intentar retirar 2300 de la cuenta1
             cuenta1.retirar(2300);
         } catch (Exception e) {
             System.out.print("Fallo al retirar");
         }
+        
         try {
+            // Mostrar mensaje de ingreso en cuenta
             System.out.println("Ingreso en cuenta");
+            
+            // Intentar ingresar la cantidad especificada en la cuenta1
             cuenta1.ingresar(cantidad);
         } catch (Exception e) {
             System.out.print("Fallo al ingresar");
@@ -26,6 +32,7 @@ public class Main {
     }
 
     public static void main(String[] args) {
+        // Llamar al método operativa_cuenta y pasas un valor.
         operativa_cuenta(695);
     }
 }
